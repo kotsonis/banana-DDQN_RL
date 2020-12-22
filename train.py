25511b6c5ba6e65442e733b2643bffc052bb8c61
kotsonis <stefankotsonis@gmail.com>
2020-12-22 02:01:35 +0100

    CLI interface to train.py

--- train.py
+++ train.py
@@ -16,11 +16,12 @@ std_learn_params = {
         # Unity Environment parameters
         "banana_location": "./Banana_Windows_x86_64/Banana.exe",
         # MDP learning parameters
-        "n_episodes": 2000,  # maximum episodes to train on
+        "n_episodes": 2000, # maximum episodes to train on
         "max_t":1000,       # maximum scenes in an episodic training
         "eps_start":1.0,    # starting exploration factor
         "eps_end":0.01,     # ending exploration factor
-        "eps_decay":0.98,  # eps step decay
+        "eps_decay":0.98,   # eps step decay
+        'early_stop': 13,   # early stop if average reward in 100 episode reaches this value
         
         # Q value learning parameters
         "gamma": 0.99,      # discount factor
@@ -29,41 +30,37 @@ std_learn_params = {
         "update_every": 4,  # how often to update the network
         
         # Replay Buffer / Prioritized Replay Buffer parameters
-        "buffer_size": 1e5, # replay buffer size
-        "batch_size": 64,   # minibatch size
-        "alpha": 0.6,       # prioritization factor (0: No prioritization .. 1: Full prioritization)
-        "pr_eps": 1e-05,    # minimum prioritization
+        "buffer_size": 1e5,         # replay buffer size
+        "batch_size": 64,           # minibatch size
+        "alpha": 0.6,               # prioritization factor (0: No prioritization .. 1: Full prioritization)
+        "pr_eps": 1e-05,            # minimum prioritization
         "beta":0.4,                 # Importance sampling beta factor start
         "beta_step": 0.00025/4.0,   # beta decay factor
         "beta_max": 1.0             # maximum beta
     }
+def moving_average(x, w):
+    vals = np.array(x)
+    return np.convolve(vals, np.ones(w), 'valid') / w
 
-def get_env_dict(env):
-    """create a dictionary with the necessary UnityEnvironment data """
-    
-    
-    return learn_env
-
-def dqn(env, learn_dict, log_results=True):
+def train_dqn(env, learn_dict, agent, log_results=True):
     """Deep Q-Learning.
     
     Params
     ======
-        n_episodes (int): maximum number of training episodes
-        max_t (int): maximum number of timesteps per episode
-        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
-        eps_end (float): minimum value of epsilon
-        eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
+        env (object):       the unity environment
+        learn_dict (dict):  learning and environment hyperparameters
+        agent (object):     starting value of epsilon, for epsilon-greedy action selection
     """
     scores = []                        # list containing scores from each episode
     scores_window = deque(maxlen=100)  # last 100 scores
-    eps = learn_dict['eps_start']                    # initialize epsilon
+    eps = learn_dict['eps_start']
     brain_name = learn_dict['brain_name']
     n_episodes=learn_dict['n_episodes']
     max_t= learn_dict['max_t']
     eps_start= learn_dict['eps_start']
     eps_end= learn_dict['eps_end']
     eps_decay= learn_dict['eps_decay']
+    early_stop = learn_dict['early_stop']
 
     for i_episode in range(1, n_episodes+1):
         env_info = env.reset(train_mode=True)[brain_name] # reset the environment
@@ -76,6 +73,7 @@ def dqn(env, learn_dict, log_results=True):
             next_state = env_info.vector_observations[0]   # get the next state
             reward = env_info.rewards[0]                   # get the reward
             done = env_info.local_done[0]                  # see if episode has finished
+            # have the agent learn a step
             agent.step(state, action, reward, next_state, done)
             state = next_state
             score += reward
@@ -87,9 +85,8 @@ def dqn(env, learn_dict, log_results=True):
         if log_results: print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
         if i_episode % 100 == 0:
             if log_results: print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
-        if np.mean(scores_window)>=50.0:
+        if np.mean(scores_window)>=early_stop:
             if log_results: print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
-            torch.save(agent.qnetwork_local.state_dict(), 'checkpoint.pth')
             break
     return scores
 
@@ -102,16 +99,34 @@ def m_a_scores(window_size, raw_scores):
         ma_scores.append(
             beta*ma_scores[i-1]+(1-beta)*raw_scores[i])
     return ma_scores
-def generate_label(params):
-    label = 'DDQN_' \
-            +'bs_' + str(learn_env['batch_size']) \
-            +'g_' + str(learn_env['gamma']) \
-            +'e_'+str(learn_env['eps_decay']) \
-            +'t_'+str(learn_env['tau']) \
-            +'lr_' + str(learn_env['lr']) \
-            +'a_' + str(learn_env['alpha']) \
-            +'b_' + str(learn_env['beta'])
-    return label
+
+def usage():
+    print(
+        "trainer.py - train the agent to solve the Banana environment\n",
+        "optional parameters: \n",
+        "--------------------- Unity Environment -------------------\n",
+        "-- banana_location <location of banana environment>\n",
+        "\n",
+        "---------------- Agent training parameters ----------------\n",
+        "--eps-start <starting value of exploration>                default: 1.0\n",
+        "--eps-decay <eps linear decay rate>                        default: 0.98\n",
+        "--episodes <number of episodes to train on>                default: 2000\n",
+        "--reward-early-stop <average reward/100 episodes>          default: 13\n",
+        "\n",
+        "-------------- Q Network learning parameters ---------------\n",
+        "--tau <Q network soft update factor>                       defalut: 5e-4\n"
+        "--gamma <factor to diminish future rewards>                default: 0.99\n",
+        "\n",
+        "--------- Prioritized Experience Replay parameters --------\n",
+        "--batch-size <mini batch size>                             default: 64\n",
+        "--beta_start <beta factor for IS weights>                  default: 0.4\n",
+        "--beta-decay <linear decay factor>                         default: 6.25e-6\n",
+        "--alpha <level of prioritization>                          default: 0.6\n",
+        "\n",
+        "--------------- storing outputs parameters ---------------\n",
+        "--output-param-file <fname to save Q_function model>       default: none\n",
+        "--output-image <png fname to save training scores plot>    default: none\n",
+    )
 
 def parse_params(argv):
     try:
@@ -126,10 +141,12 @@ def parse_params(argv):
                                        "output-param-file=",
                                        "output-image=",
                                        "gamma=",
+                                       "tau=",
                                        "beta_start=",
                                        "beta-decay=",
                                        "alpha=",
-                                       "banana_location="
+                                       "banana_location=",
+                                       "help"
                                     ])
     except getopt.GetoptError as err:
         # print help information and exit:
@@ -139,32 +156,35 @@ def parse_params(argv):
     hyperparams = std_learn_params
 
     for o, a in opts:
-        if o == "eps-start=":
-            hyperparams['eps_start'] = a
-        elif o == "eps-decay=":
-            hyperparams['eps_decay'] = a
-        elif o == "batch-size=":
-            hyperparams['batch_size'] = a
-        elif o == "episodes=":
-            hyperparams['num_episodes']=a
-        elif o == "reward-early-stop=":
-            hyperparams['early_stop'] = a
-        elif o == "output-param-file=":
+        if o == "--eps-start":
+            hyperparams['eps_start'] = float(a)
+        elif o == "--eps-decay":
+            hyperparams['eps_decay'] = float(a)
+        elif o == "--batch-size":
+            hyperparams['batch_size'] = int(a)
+        elif o == "--episodes":
+            hyperparams['n_episodes']= int(a)
+        elif o == "--reward-early-stop":
+            hyperparams['early_stop'] = float(a)
+        elif o == "--tau":
+            hyperparams['tau'] = float(a)
+        elif o == "--output-param-file":
             hyperparams['model_file'] = a
-        elif o == "output-image=":
+        elif o == "--output-image":
             hyperparams['plt_file'] = a
-        elif o == "gamma=":
-            hyperparams['gamma'] = a
-        elif o == "beta-start=":
-            hyperparams['beta']=a
-        elif o == "beta-decay=":
-            hyperparams['beta_decay']=a
-        elif o == "alpha=":
-            hyperparams['alpha'] = a
-        elif o in ("-b", "banana_location="):
+        elif o == "--gamma":
+            hyperparams['gamma'] = float(a)
+        elif o == "--beta-start":
+            hyperparams['beta']=float(a)
+        elif o == "--beta-decay":
+            hyperparams['beta_decay']=float(a)
+        elif o == "--alpha":
+            hyperparams['alpha'] = float(a)
+        elif o in ("-b", "--banana_location"):
             hyperparams['banana_location']=a
-        elif o == "h":
+        elif o == "--help":
             usage()
+            sys.exit(2)
         else:
             assert False, "unhandled option"
     # return the modified hyperparams
@@ -178,49 +198,42 @@ def main(argv):
     brain = env.brains[brain_name]
     env_info = env.reset(train_mode=True)[brain_name]
 
-    # set our learning enviroment hyperparameters
-    learn_env = {
-        # Unity Environment
-        "brain_name" : brain_name,
-        "brain" : brain,
-        "action_size":  brain.vector_action_space_size,
-        "state_size": len(env_info.vector_observations[0]),
-    }
-
-    i = 0
-    ma_scores = []
-    labels = []
+    # update hyperparameters with Unity Environment
+    learn_env["brain_name"] = brain_name
+    learn_env["brain"] = brain
+    learn_env["action_size"] = brain.vector_action_space_size
+    learn_env["state_size"] = len(env_info.vector_observations[0])
+    
     agent = DDQNPrioritizedAgent(learn_env)
-    scores = dqn(env, learn_env)
-    labels.append(generate_label(learn_env))
-    ma_scores.m_a_scores(10,scores)
-
-    """learn_env['batch_size'] = batch_size
-    print("Starting: ", generate_label(learn_env))
-    agent = DDQNPrioritizedAgent(learn_env)
-    scores = dqn(env, learn_env)
-    labels.append(generate_label(learn_env))
-    ma_scores.append(m_a_scores(10,scores)) """
+    scores = train_dqn(env, learn_env, agent)
+    label = "10 episode moving average score"
+    ma_window_size = 10
+    ma_scores = moving_average(scores,ma_window_size)
+    
+    torch_model_fname = learn_env.get('model_file')
+    if(torch_model_fname):
+        torch.save(agent.qnetwork_local.state_dict(), torch_model_fname)
+    
 
     #plot results
-    plt.figure(figsize=(11.75,8.25))
+    plt.figure()
     plt.title('Mean Score')
     plt.xlabel("Episodes")
     plt.ylabel("Mean Score")
-    episodes=range(learn_env['n_episodes'])
-    for j in range(len(ma_scores)):
-        plt.plot(episodes, ma_scores[j], label=labels[j])
+    episodes=range(len(ma_scores))
+    plt.plot(episodes, ma_scores, label=label)
     
     plt.legend(loc='lower right')
-    f_name = labels[0] + '.png'
-    plt.savefig(f_name)
-    plt.show()
-    
+    f_name = learn_env.get('plt_file')
+    if (f_name):
+        plt.savefig(f_name)
+        plt.show()
+    else:
+        plt.show()
     env.close()
-if __name__ == "__main__":
-   main(sys.argv[1:])
 
 if __name__ == "__main__":
+   main(sys.argv[1:])
 
 
 
